a351ff2b0bf753182e1a150255774b819e15c96a
Bas Janssen <smh.janssen@gmail.com>
2018-12-28 17:13:08 +0100

    Extended DE8 generator

--- EuroJudoWebContestSheets/Controllers/TournamentsController.cs
+++ EuroJudoWebContestSheets/Controllers/TournamentsController.cs
@@ -36,15 +36,51 @@ namespace EuroJudoWebContestSheets.Controllers
                 {
                     new ContestSheetData
                     {
-                        Contest = 9,
+                        Contest = 1,
                         CompeditorWhite = "MARTIN, Mauro",
                         CompeditorBlue = "VOLLEMAN, Tibo"
                     },
                     new ContestSheetData
                     {
-                        Contest = 10,
+                        Contest = 2,
                         CompeditorWhite = "LICHTENBERG, Bram",
                         CompeditorBlue = "SCHREURS, Vincent"
+                    },
+                    new ContestSheetData
+                    {
+                        Contest = 3,
+                        CompeditorWhite = "JANSSEN, Bas",
+                        CompeditorBlue = "PIETERS, Jan"
+                    },
+                    new ContestSheetData
+                    {
+                        Contest = 4,
+                        CompeditorWhite = "KLAASSEN, Rob",
+                        CompeditorBlue = "VAN DIJK, Nico"
+                    },
+                    new ContestSheetData
+                    {
+                        Contest = 5,
+                        CompeditorWhite = "MARTIN, Mauro",
+                        CompeditorBlue = "SCHREURS, Vincent"
+                    },
+                    new ContestSheetData
+                    {
+                        Contest = 6,
+                        CompeditorWhite = "PIETERS, Jan",
+                        CompeditorBlue = "KLAASSEN, Rob"
+                    },
+                    new ContestSheetData
+                    {
+                        Contest = 7,
+                        CompeditorWhite = "VOLLEMAN, Tibo",
+                        CompeditorBlue = "LICHTENBERG, Bram"
+                    },
+                    new ContestSheetData
+                    {
+                        Contest = 8,
+                        CompeditorWhite = "JANSSEN, Bas",
+                        CompeditorBlue = "VAN DIJK, Nico"
                     }
                 }
             };
@@ -57,6 +93,14 @@ namespace EuroJudoWebContestSheets.Controllers
         {
             List<ContestSheetData> sheetData = _db.ContestSheetData.Where(o => o.TournamentID == TournamentId).ToList();
             return new JsonResult(sheetData);
+        }
+
+        [HttpPost]
+        public IActionResult PostContestSheetData([FromBody] ContestSheetData contestData)
+        {
+            //List<ContestSheetData> sheetData = _db.ContestSheetData.Where(o => o.TournamentID == TournamentId).ToList();
+            //return new JsonResult(sheetData);
+            return Ok();
         }
     }
 }
--- EuroJudoWebContestSheets/Generators/DoubleElimination8.cs
+++ EuroJudoWebContestSheets/Generators/DoubleElimination8.cs
@@ -23,15 +23,15 @@ namespace EuroJudoWebContestSheets.Generators
             string SVG = "";
 
             SVG = SVG + "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-            SVG = SVG + "<svg viewBox=\"150 50 1100 450\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">";
+            SVG = SVG + "<svg viewBox=\"140 50 1100 900\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">";
 
             //Draw the left of the 8 person sheet, main bracket
             SVG = SVG + "<line x1=\"170\" y1=\"75\" x2=\"320\" y2=\"75\" stroke=\"black\" stroke-width=\"1\" />";
             SVG = SVG + "<line x1=\"170\" y1=\"185\" x2=\"320\" y2=\"185\" stroke=\"black\" stroke-width=\"1\" />";
             SVG = SVG + "<line x1=\"170\" y1=\"295\" x2=\"320\" y2=\"295\" stroke=\"black\" stroke-width=\"1\" />";
             SVG = SVG + "<line x1=\"170\" y1=\"405\" x2=\"320\" y2=\"405\" stroke=\"black\" stroke-width=\"1\" />";
-            //Rewrite to check if the result is not null, then use the object to get the compeditors.
-            ContestSheetData contest = category.SheetData.Where(o => o.Contest == 9).FirstOrDefault();
+            
+            ContestSheetData contest = category.SheetData.Where(o => o.Contest == 1).FirstOrDefault();
             if (contest == null)
             {
                 SVG = SVG + "<text x=\"180\" y=\"65\" fill=\"black\" id=\"QFA1\"></text>";
@@ -43,7 +43,7 @@ namespace EuroJudoWebContestSheets.Generators
                 SVG = SVG + "<text x=\"180\" y=\"175\" fill=\"black\" id=\"QFA2\">" + contest.CompeditorBlue + "</text>";
             }
 
-            contest = category.SheetData.Where(o => o.Contest == 10).FirstOrDefault();
+            contest = category.SheetData.Where(o => o.Contest == 2).FirstOrDefault();
             if (contest == null)
             {
                 SVG = SVG + "<text x=\"180\" y=\"285\" fill=\"black\" id=\"QFA3\"></text>";
@@ -61,7 +61,7 @@ namespace EuroJudoWebContestSheets.Generators
             SVG = SVG + "<line x1=\"320\" y1=\"130\" x2=\"470\" y2=\"130\" stroke=\"black\" stroke-width=\"1\" />";
             SVG = SVG + "<line x1=\"320\" y1=\"350\" x2=\"470\" y2=\"350\" stroke=\"black\" stroke-width=\"1\" />";
 
-            contest = category.SheetData.Where(o => o.Contest == 21).FirstOrDefault();
+            contest = category.SheetData.Where(o => o.Contest == 5).FirstOrDefault();
             if (contest == null)
             {
                 SVG = SVG + "<text x=\"330\" y=\"120\" fill=\"black\" id=\"semifinalA1\"></text>";
@@ -83,18 +83,49 @@ namespace EuroJudoWebContestSheets.Generators
             SVG = SVG + "<line x1=\"1090\" y1=\"185\" x2=\"940\" y2=\"185\" stroke=\"black\" stroke-width=\"1\" />";
             SVG = SVG + "<line x1=\"1090\" y1=\"295\" x2=\"940\" y2=\"295\" stroke=\"black\" stroke-width=\"1\" />";
             SVG = SVG + "<line x1=\"1090\" y1=\"405\" x2=\"940\" y2=\"405\" stroke=\"black\" stroke-width=\"1\" />";
-            SVG = SVG + "<text x=\"950\" y=\"65\" fill=\"black\" id=\"QFB1\"></text>";
-            SVG = SVG + "<text x=\"950\" y=\"175\" fill=\"black\" id=\"QFB2\"></text>";
-            SVG = SVG + "<text x=\"950\" y=\"285\" fill=\"black\" id=\"QFB3\"></text>";
-            SVG = SVG + "<text x=\"950\" y=\"395\" fill=\"black\" id=\"QFB4\"></text>";
+
+            contest = category.SheetData.Where(o => o.Contest == 3).FirstOrDefault();
+            if (contest == null)
+            {
+                SVG = SVG + "<text x=\"950\" y=\"65\" fill=\"black\" id=\"QFB1\"></text>";
+                SVG = SVG + "<text x=\"950\" y=\"175\" fill=\"black\" id=\"QFB2\"></text>";
+            }
+            else
+            {
+                SVG = SVG + "<text x=\"950\" y=\"65\" fill=\"black\" id=\"QFB1\">" + contest.CompeditorWhite + "</text>";
+                SVG = SVG + "<text x=\"950\" y=\"175\" fill=\"black\" id=\"QFB2\">" + contest.CompeditorBlue + "</text>";
+            }            
+
+            contest = category.SheetData.Where(o => o.Contest == 4).FirstOrDefault();
+            if (contest == null)
+            {
+                SVG = SVG + "<text x=\"950\" y=\"285\" fill=\"black\" id=\"QFB3\"></text>";
+                SVG = SVG + "<text x=\"950\" y=\"395\" fill=\"black\" id=\"QFB4\"></text>";
+            }
+            else
+            {
+                SVG = SVG + "<text x=\"950\" y=\"285\" fill=\"black\" id=\"QFB4\">" + contest.CompeditorWhite + "</text>";
+                SVG = SVG + "<text x=\"950\" y=\"395\" fill=\"black\" id=\"QFB3\">" + contest.CompeditorBlue + "</text>";
+            }
+            
 
             SVG = SVG + "<line x1=\"940\" y1=\"75\" x2=\"940\" y2=\"185\" stroke=\"black\" stroke-width=\"1\" />";
             SVG = SVG + "<line x1=\"940\" y1=\"295\" x2=\"940\" y2=\"405\" stroke=\"black\" stroke-width=\"1\" />";
 
             SVG = SVG + "<line x1=\"940\" y1=\"130\" x2=\"790\" y2=\"130\" stroke=\"black\" stroke-width=\"1\" />";
             SVG = SVG + "<line x1=\"940\" y1=\"350\" x2=\"790\" y2=\"350\" stroke=\"black\" stroke-width=\"1\" />";
-            SVG = SVG + "<text x=\"800\" y=\"120\" fill=\"black\" id=\"semifinalB1\"></text>";
-            SVG = SVG + "<text x=\"800\" y=\"340\" fill=\"black\" id=\"semifinalB2\"></text>";
+
+            contest = category.SheetData.Where(o => o.Contest == 6).FirstOrDefault();
+            if (contest == null)
+            {
+                SVG = SVG + "<text x=\"800\" y=\"120\" fill=\"black\" id=\"semifinalB1\"></text>";
+                SVG = SVG + "<text x=\"800\" y=\"340\" fill=\"black\" id=\"semifinalB2\"></text>";
+            }
+            else
+            {
+                SVG = SVG + "<text x=\"800\" y=\"120\" fill=\"black\" id=\"semifinalB1\">" + contest.CompeditorWhite + "</text>";
+                SVG = SVG + "<text x=\"800\" y=\"340\" fill=\"black\" id=\"semifinalB2\">" + contest.CompeditorBlue + "</text>";
+            }
 
             SVG = SVG + "<line x1=\"790\" y1=\"130\" x2=\"790\" y2=\"350\" stroke=\"black\" stroke-width=\"1\" />";
 
@@ -109,7 +140,107 @@ namespace EuroJudoWebContestSheets.Generators
             SVG = SVG + "<text x=\"1092\" y=\"190\" fill=\"black\">6</text>";
             SVG = SVG + "<text x=\"1092\" y=\"300\" fill=\"black\">7</text>";
             SVG = SVG + "<text x=\"1092\" y=\"410\" fill=\"black\">8</text>";
-           
+
+
+            SVG = SVG + "<line x1=\"940\" y1=\"75\" x2=\"940\" y2=\"185\" stroke=\"black\" stroke-width=\"1\" />";
+            SVG = SVG + "<line x1=\"940\" y1=\"295\" x2=\"940\" y2=\"405\" stroke=\"black\" stroke-width=\"1\" />";
+
+            SVG = SVG + "<line x1=\"940\" y1=\"130\" x2=\"790\" y2=\"130\" stroke=\"black\" stroke-width=\"1\" />";
+            SVG = SVG + "<line x1=\"940\" y1=\"350\" x2=\"790\" y2=\"350\" stroke=\"black\" stroke-width=\"1\" />";
+
+
+
+
+            //Repachage
+
+            SVG = SVG + "<line x1=\"170\" y1=\"505\" x2=\"320\" y2=\"505\" stroke=\"black\" stroke-width=\"1\" />";
+            SVG = SVG + "<line x1=\"170\" y1=\"615\" x2=\"320\" y2=\"615\" stroke=\"black\" stroke-width=\"1\" />";
+            
+            contest = category.SheetData.Where(o => o.Contest == 7).FirstOrDefault();
+            if (contest == null)
+            {
+                SVG = SVG + "<text x=\"180\" y=\"495\" fill=\"black\" id=\"RA1\"></text>";
+                SVG = SVG + "<text x=\"180\" y=\"605\" fill=\"black\" id=\"RA2\"></text>";
+            }
+            else
+            {
+                SVG = SVG + "<text x=\"180\" y=\"495\" fill=\"black\" id=\"RA1\">" + contest.CompeditorWhite + "</text>";
+                SVG = SVG + "<text x=\"180\" y=\"605\" fill=\"black\" id=\"RA2\">" + contest.CompeditorBlue + "</text>";
+            }
+
+            SVG = SVG + "<line x1=\"320\" y1=\"505\" x2=\"320\" y2=\"615\" stroke=\"black\" stroke-width=\"1\" />";
+
+            SVG = SVG + "<line x1=\"320\" y1=\"560\" x2=\"470\" y2=\"560\" stroke=\"black\" stroke-width=\"1\" />";
+            SVG = SVG + "<line x1=\"320\" y1=\"670\" x2=\"470\" y2=\"670\" stroke=\"black\" stroke-width=\"1\" />";
+
+            contest = category.SheetData.Where(o => o.Contest == 9).FirstOrDefault();
+            if (contest == null)
+            {
+                SVG = SVG + "<text x=\"330\" y=\"550\" fill=\"black\" id=\"repachageFinalA1\"></text>";
+                SVG = SVG + "<text x=\"330\" y=\"660\" fill=\"black\" id=\"repachageFinalA2\"></text>";
+            }
+            else
+            {
+                SVG = SVG + "<text x=\"330\" y=\"550\" fill=\"black\" id=\"repachageFinalA1\">" + contest.CompeditorWhite + "</text>";
+                SVG = SVG + "<text x=\"330\" y=\"660\" fill=\"black\" id=\"repachageFinalA2\">" + contest.CompeditorBlue + "</text>";
+            }
+
+            SVG = SVG + "<line x1=\"470\" y1=\"560\" x2=\"470\" y2=\"670\" stroke=\"black\" stroke-width=\"1\" />";
+
+            SVG = SVG + "<line x1=\"470\" y1=\"615\" x2=\"620\" y2=\"615\" stroke=\"black\" stroke-width=\"1\" />";
+            SVG = SVG + "<text x=\"480\" y=\"605\" fill=\"black\" id=\"repachageA\"></text>";
+
+            //Draw the right of the 8 person sheet, main bracket
+            SVG = SVG + "<line x1=\"1090\" y1=\"505\" x2=\"940\" y2=\"505\" stroke=\"black\" stroke-width=\"1\" />";
+            SVG = SVG + "<line x1=\"1090\" y1=\"615\" x2=\"940\" y2=\"615\" stroke=\"black\" stroke-width=\"1\" />";
+            
+            contest = category.SheetData.Where(o => o.Contest == 8).FirstOrDefault();
+            if (contest == null)
+            {
+                SVG = SVG + "<text x=\"950\" y=\"495\" fill=\"black\" id=\"RB1\"></text>";
+                SVG = SVG + "<text x=\"950\" y=\"605\" fill=\"black\" id=\"RB2\"></text>";
+            }
+            else
+            {
+                SVG = SVG + "<text x=\"950\" y=\"495\" fill=\"black\" id=\"RB1\">" + contest.CompeditorWhite + "</text>";
+                SVG = SVG + "<text x=\"950\" y=\"605\" fill=\"black\" id=\"RB2\">" + contest.CompeditorBlue + "</text>";
+            }
+
+            SVG = SVG + "<line x1=\"940\" y1=\"505\" x2=\"940\" y2=\"615\" stroke=\"black\" stroke-width=\"1\" />";
+
+            SVG = SVG + "<line x1=\"940\" y1=\"560\" x2=\"790\" y2=\"560\" stroke=\"black\" stroke-width=\"1\" />";
+            SVG = SVG + "<line x1=\"940\" y1=\"670\" x2=\"790\" y2=\"670\" stroke=\"black\" stroke-width=\"1\" />";
+
+            contest = category.SheetData.Where(o => o.Contest == 10).FirstOrDefault();
+            if (contest == null)
+            {
+                SVG = SVG + "<text x=\"800\" y=\"550\" fill=\"black\" id=\"semifinalB1\"></text>";
+                SVG = SVG + "<text x=\"800\" y=\"660\" fill=\"black\" id=\"semifinalB2\"></text>";
+            }
+            else
+            {
+                SVG = SVG + "<text x=\"800\" y=\"550\" fill=\"black\" id=\"repachageFinalB1\">" + contest.CompeditorWhite + "</text>";
+                SVG = SVG + "<text x=\"800\" y=\"660\" fill=\"black\" id=\"repachageFinalB2\">" + contest.CompeditorBlue + "</text>";
+            }
+
+            SVG = SVG + "<line x1=\"790\" y1=\"560\" x2=\"790\" y2=\"670\" stroke=\"black\" stroke-width=\"1\" />";
+
+            SVG = SVG + "<line x1=\"640\" y1=\"615\" x2=\"790\" y2=\"615\" stroke=\"black\" stroke-width=\"1\" />";
+            SVG = SVG + "<text x=\"650\" y=\"605\" fill=\"black\" id=\"repachageB\"></text>";
+
+            SVG = SVG + "<text x=\"148\" y=\"510\" fill=\"black\">R1</text>";
+            SVG = SVG + "<text x=\"148\" y=\"620\" fill=\"black\">R2</text>";
+            SVG = SVG + "<text x=\"1098\" y=\"510\" fill=\"black\">R3</text>";
+            SVG = SVG + "<text x=\"1098\" y=\"620\" fill=\"black\">R4</text>";
+
+
+            SVG = SVG + "<line x1=\"940\" y1=\"75\" x2=\"940\" y2=\"185\" stroke=\"black\" stroke-width=\"1\" />";
+            SVG = SVG + "<line x1=\"940\" y1=\"295\" x2=\"940\" y2=\"405\" stroke=\"black\" stroke-width=\"1\" />";
+
+            SVG = SVG + "<line x1=\"940\" y1=\"130\" x2=\"790\" y2=\"130\" stroke=\"black\" stroke-width=\"1\" />";
+            SVG = SVG + "<line x1=\"940\" y1=\"350\" x2=\"790\" y2=\"350\" stroke=\"black\" stroke-width=\"1\" />";
+
+
             SVG = SVG + "</svg>";
 
             _Image = SVG;
