661c55c46fb89a270545468016720b12a55953c6
Bas Janssen <BasJ93@users.noreply.github.com>
2019-03-02 17:04:15 +0100

    Initial code for a publisher tool.
    The tool connects as if it is an scoreboard, and publishes the data to the webserver.

---
+++ EJUPublisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace EJUPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int serverPort = 50228;
+
+            TcpClient client = new TcpClient("127.0.0.1", serverPort);
+            NetworkStream _stream = client.GetStream();
+
+            while(client.Connected)
+            {
+                var buffer = new Byte[256];
+                _stream.Read(buffer, 0, 255);
+                string command = System.Text.Encoding.ASCII.GetString(buffer, 0, 255);
+                if(command.Contains("^ID"))
+                {
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes("!OC0010061006       D.");
+                    _stream.Write(data, 0, data.Length);
+                    Console.Write($"ID received, send response. {command}");
+                    Console.Write($"Responded with: {System.Text.Encoding.ASCII.GetString(data, 0, data.Length)}");
+                }
+                else
+                {
+                    Console.Write(command);
+                }
+            }
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
