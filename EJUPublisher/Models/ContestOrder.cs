a61340246b2c8fefd835ce98595656836d9aa076
Bas Janssen <BasJ93@users.noreply.github.com>
2019-03-15 22:06:36 +0100

    Add config trough appsettings.json

---
+++ EJUPublisher/Models/ContestOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EJUPublisher.Models
+{
+    public class ContestOrder
+    {
+        public int Tatami { get; set; }
+        public List<Contest> Contests { get; set; }
+
+        public ContestOrder()
+        {
+            Contests = new List<Contest>();
+        }
+    }
+}
--- EJUPublisher/Program.cs
+++ EJUPublisher/Program.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using EJUPublisher.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Net.Sockets;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace EJUPublisher
 {
@@ -9,17 +15,41 @@ namespace EJUPublisher
     {
         static void Main(string[] args)
         {
-            int serverPort = 50228;
 
-            int numberOfTatami = 8;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
-            TcpClient client = new TcpClient("192.168.2.3", serverPort);
-            NetworkStream _stream = client.GetStream();
+            IConfigurationRoot configuration = builder.Build();
 
-            while(client.Connected)
+            Console.WriteLine(configuration.GetConnectionString("Storage"));
+
+            int serverPort = Convert.ToInt32(configuration["EJUPort"]);
+
+            int numberOfTatami = Convert.ToInt32(configuration["NumberOfTatami"]);
+
+            int numberOfContests = Convert.ToInt32(configuration["NumberOfContests"]);
+
+            string EJUServer = configuration["EJUServer"];
+
+            string WebServer = configuration["WebServer"];
+
+            HttpClient _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", "EJUPublisher");
+
+            List<ContestOrder> contestOrder = new List<ContestOrder>();
+            for(int i=0; i<numberOfTatami; i++)
             {
-                var buffer = new Byte[1500 * numberOfTatami];
-                _stream.Read(buffer, 0, 1500 * numberOfTatami);
+                contestOrder.Add(new ContestOrder() { Tatami = i + 1});
+            }
+
+            TcpClient HTTPClient = new TcpClient(EJUServer, serverPort);
+            NetworkStream _stream = HTTPClient.GetStream();
+
+            while(HTTPClient.Connected)
+            {
+                var buffer = new Byte[2048 * numberOfTatami];
+                _stream.Read(buffer, 0, 2048 * numberOfTatami);
                 string command = System.Text.Encoding.ASCII.GetString(buffer, 0, buffer.Length);
                 if(command.Contains("^ID"))
                 {
@@ -35,14 +65,54 @@ namespace EJUPublisher
                     command = command.Substring(0, command.IndexOf('\0'));
                     List<string> contests = command.Split("\n").ToList();
                     Console.WriteLine($"Data received at {DateTime.Now.ToLongTimeString()} consisting of {contests.Count} contests:");
+
+                    foreach(var tatami in contestOrder)
+                    {
+                        tatami.Contests.Clear();
+                    }
+
+                    //Convert to contestOrder lists
                     foreach (var contest in contests)
                     {
                         Console.WriteLine(contest);
+                        var _contest = EJU2Contest(contest);
+                        if (_contest.Tatami > 0)
+                        {
+                            if (contestOrder[_contest.Tatami - 1].Contests.Count < numberOfContests)
+                            {
+                                contestOrder[_contest.Tatami - 1].Contests.Add(_contest);
+                            }
+                        }
                     }
+
+                    //Upload to webserver
+                    StringContent requestBody = new StringContent(JsonConvert.SerializeObject(contestOrder), Encoding.UTF8, "application/json");
+                    _httpClient.PostAsync(WebServer, requestBody);
+					Console.WriteLine($"JSON:{JsonConvert.SerializeObject(contestOrder)}");
                 }
             }
+        }
 
-            //Console.WriteLine("Hello World!");
+        private static Contest EJU2Contest(string EJU)
+        {
+            if (EJU.Contains(';'))
+            {
+                List<string> components = EJU.Split(';').ToList();
+                Contest _contest = new Contest()
+                {
+                    Tatami = Convert.ToInt32(components[0]),
+                    Number = Convert.ToInt32(components[1]),
+                    Weight = components[2],
+                    CompeditorWhite = components[3],
+                    CompeditorBlue = components[7]
+                };
+
+                return _contest;
+            }
+            else
+            {
+                return new Contest();
+            }
         }
     }
 }
