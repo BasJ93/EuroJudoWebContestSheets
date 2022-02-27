1e0a19c71e0bbe981ada23e5260f11436c677188
Bas Janssen <bas@basjanssen.eu>
2022-02-27 11:16:27 +0000

    Add short and weight to Contest model

---
+++ EuroJudoWebContestSheets/Migrations/20220225155054_CategoryShortAndWeight.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EuroJudoWebContestSheets.Migrations
+{
+    public partial class CategoryShortAndWeight : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Short",
+                table: "Categories",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Weight",
+                table: "Categories",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Short",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "Categories");
+        }
+    }
+}
